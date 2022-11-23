a5b35afce120ebc2a47b7244b9a876dc91b7a6e9
Isaac <isaacaldairavila@gmail.com>
2022-11-23 13:45:43 -0500

    chromatic

--- src/stories/card/Card.jsx
+++ src/stories/card/Card.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import styles from './Card.module.css'
 /**
  * Primary UI component for user interaction
  */
 export const Card = ({label, ...props }) => {
   return (
     <button
+    className={styles.card}
       type="button"
       {...props}
     >
